383f645d7d8f384827ad1b28700975eb0ffb6af2
Ibrahim Sma <ibrahim.sma1990@gmail.com>
2024-04-02 12:27:25 +0530

    Merging local changes

--- 4_SquareRoot.py
+++ 4_SquareRoot.py
@@ -1,9 +1,5 @@
 """
-<<<<<<< HEAD
 4) Write a program that calculates and prints the value according to the given formula:
-=======
-6) Write a program that calculates and prints the value according to the given formula:
->>>>>>> 026b5d111632615c3afdf3da663e99480c91f47b
 Q = Square root of [(2 * C * D)/H] Following are the fixed values of C and H:  C is 50. H is 30.
 D is the variable whose values should be input to your program in a comma-separated sequence.
 Example: Let us assume the following comma separated input sequence is given to the program:
